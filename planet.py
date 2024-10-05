1addecefa0187a07407d7e84e157b07cc34965b6
Ali Hakimi <alihakimi014@gmai.com>
2024-10-05 02:59:16 -0600

    add orbit

--- planet.py
+++ planet.py
@@ -21,6 +21,17 @@ class Planet:
     def draw(self, win):
         x = self.x * SCALE + WIDTH / 2
         y = self.y * SCALE + HEIGHT / 2
+
+        if len(self.orbit) > 2:
+            updated_points = []
+            for point in self.orbit:
+                x, y = point
+                x = x * SCALE + WIDTH / 2
+                y = y * SCALE + HEIGHT / 2
+                updated_points.append((x, y))
+
+            pygame.draw.lines(win, self.color, False, updated_points, 2)
+        
         pygame.draw.circle(win, self.color, (x,y), self.radius)
 
     def attraction(self, other):
